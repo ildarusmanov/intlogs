766a6bad1438365e82f8784bc5029069ddc03dd6
Ildar Usmanov <wajox@mail.ru>
2017-08-17 17:46:59 +0300

    add Cost field into ActionLog model

--- models/actionlog.go
+++ models/actionlog.go
@@ -4,6 +4,7 @@ type ActionLog struct {
 	Id           string	`bson:"_id,omitempty"`
 	ActionName   string	`bson:"action_name"`
 	ActionTarget string	`bson:"action_target"`
+	ActionCost   int	`bson:"action_cost"`
 	UserId       string	`bson:"user_id"`
 	GuestUserId  string	`bson:"guest_user_id"`
 	Url          string	`bson:"url"`
